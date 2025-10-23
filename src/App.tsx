e486877ce06237c2d9f942b66351b5cdf8237029
streamline-autmations <streamline.automations.hq@gmail.com>
2025-10-23 09:28:46 +0200

    Fix routing - redirect incomplete profiles to account-setup instead of login

--- src/App.tsx
+++ src/App.tsx
@@ -84,7 +84,7 @@ const AppRoutes = () => {
         <Route path="/account-setup" element={<SetupProfile />} />
         <Route path="/login" element={<Login />} />
         <Route path="/accept-invite" element={<AcceptInvite />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/account-setup" replace />} />
       </Routes>
     );
   }
